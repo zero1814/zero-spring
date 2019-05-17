0c9ba2bb83bf109869aa93578e8568f3609df902
zhanghaiyu <zhanghaiyu@reignwood.com>
2019-05-17 15:31:51 +0800

    zhy

--- zero-spring-jpa/src/main/java/org/zero/spring/jpa/BaseEntity.java
+++ zero-spring-jpa/src/main/java/org/zero/spring/jpa/BaseEntity.java
@@ -8,7 +8,6 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Transient;
 
-import com.alibaba.fastjson.annotation.JSONField;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -28,42 +27,34 @@ public class BaseEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Column(name = "uid", length = 50, unique = true, updatable = false)
-	@JSONField(serialize=false)
 	private String uid;
 
 	@Id
-	@Column(name = "code", length = 50, unique = true, updatable = false)
-	@JSONField(serialize=false)
+	@Column(name = "code", length = 50)
 	private String code;
 
 	@Column(name = "create_user", length = 50, insertable = true, updatable = false, nullable = false)
-	@JSONField(serialize=false)
 	private String createUser;
 
 	@Column(name = "create_time", insertable = true, updatable = false, nullable = false)
-	@JSONField(serialize=false)
 	private Date createTime;
 
 	@Column(name = "update_user", length = 50, insertable = true, updatable = true, nullable = false)
-	@JSONField(serialize=false)
 	private String updateUser;
 
 	@Column(name = "update_time", insertable = true, updatable = true, nullable = false)
-	@JSONField(serialize=false)
 	private Date updateTime;
 
 	/**
 	 * 当前页
 	 */
 	@Transient
-	@JSONField(serialize=false)
 	private int page;
 
 	/**
 	 * 页面显示最大数
 	 */
 	@Transient
-	@JSONField(serialize=false)
 	private int size;
 
 }
