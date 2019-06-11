d0f882fe62f7ecc827e088c376d705dfe12b2cf5
zhanghaiyu <zhanghaiyu@reignwood.com>
2019-06-11 16:41:20 +0800

    zhy

--- zero-spring-jpa/src/main/java/org/zero/spring/jpa/JpaUtil.java
+++ zero-spring-jpa/src/main/java/org/zero/spring/jpa/JpaUtil.java
@@ -9,6 +9,7 @@ import javax.persistence.Column;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 
 import zero.commons.basics.ObjectUtil;
 
@@ -17,20 +18,29 @@ public class JpaUtil<T> {
 	public static ExampleMatcher getMatcher(Object obj) {
 		try {
 			ExampleMatcher matcher = ExampleMatcher.matching();
+			// 忽略大小写
+			matcher.withIgnoreCase(true);
+			// 改变默认字符串匹配方式：模糊查询
+			matcher.withStringMatcher(StringMatcher.CONTAINING);
+			// 忽略空值
+			matcher.withIgnoreNullValues();
 			// 遍历属性值，匹配查询条件
 			List<Map<String, Object>> list = ObjectUtil.getFiledsInfo(obj);
 			if (!list.isEmpty()) {
 				for (Map<String, Object> map : list) {
+					String name = map.get("name").toString();
 					if (StringUtils.equals(map.get("type").toString(), "class java.lang.String")) {
-						String name = map.get("name").toString();
 						if (map.get("value") != null && StringUtils.isNotBlank(map.get("value").toString())) {
 							matcher.withMatcher(name, GenericPropertyMatchers.contains());
+						}
+					} else {
+						if (map.get("value") != null) {
+							matcher.withMatcher(name, GenericPropertyMatchers.startsWith());
 						}
 					}
 				}
 			}
-			// 忽略大小写
-			matcher.withIgnoreCase();
+			matcher.withIgnorePaths("flagEnabled");
 			return matcher;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -47,7 +57,7 @@ public class JpaUtil<T> {
 			for (Field field : list) {
 				Column col = field.getDeclaredAnnotation(Column.class);
 				if (col.unique()) {
-					
+
 				}
 			}
 		} catch (Exception e) {
